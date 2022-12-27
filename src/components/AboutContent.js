ac15c381cc473749afeb174675cb4acaf426a94c
carla <carlabgo95@gmail.com>
2022-12-27 15:06:03 -0300

    csharp icon

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -2,7 +2,8 @@ import "./aboutcontent.css";
 
 import React from 'react';
 import { Link } from "react-router-dom";
-import { FaCcStripe, FaCss3, FaDatabase, FaGit, FaHtml5, FaJava, FaJs, FaReact, FaSquareFull } from "react-icons/fa";
+import { FaCss3, FaDatabase, FaGit, FaHtml5, FaJava, FaJs, FaReact, FaSquareFull } from "react-icons/fa";
+import CSharp from "../assets/icons8-c-sharp-logotipo(2).svg"
 
 const AboutContent = () => {
     return (
@@ -10,7 +11,12 @@ const AboutContent = () => {
             <div className="left">
                 <h1>¿Quién soy?</h1>
                 <p>Soy una desarrolladora full stack con ganas de crecer
-                    en el mundo de la programación.
+                    en el mundo de la programación y de mejorar mis habilidades. Me
+                    considero una persona flexible, que sabe adaptarse a
+                    los cambios, que sabe trabajar en equipo y que busca
+                    constantemente aprender de otros, de mi misma y de
+                    las nuevas tecnologías. Me gustaría seguir
+                    aprendiendo y desafiándome.
                 </p>
                 <Link to="/contact">
                     <button className="btn">Contactame</button>
@@ -19,12 +25,14 @@ const AboutContent = () => {
             <div className="right">
                 <div className="icons-container">
                     <FaJs size={40} style={{ color: "white", marginRight: "1.5rem" }} />
+                    <img src={CSharp} alt="csharp" size={40} style={{ color: "white", marginRight:"1.2rem"}}></img>
                     <FaCss3 size={40} style={{ color: "white", marginRight: "1.5rem" }} />
                     <FaHtml5 size={40} style={{ color: "white", marginRight: "1.5rem" }} />
                     <FaGit size={40} style={{ color: "white", marginRight: "1.5rem" }} />
                     <FaReact size={40} style={{ color: "white", marginRight: "1.5rem" }} />
                     <FaDatabase size={40} style={{ color: "white", marginRight: "1.5rem" }} />
                     <FaJava size={40} style={{ color: "white", marginRight: "1.5rem" }} />
+                    
                 </div>
             </div>
         </div>
--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,26 +1,20 @@
-import img1 from "../assets/senor-cara-papa-actor-dobale-muerte.jpg";
+import img1 from "../assets/pro1.png";
+import img2 from "../assets/pro2.png";
 
 const ProjectCardData = [
     {
         imgsrc: img1,
-        title: "Gula Gula",
-        text: "Text text text",
-        view: "https://www.google.com/",
-        source: "https://www.google.com/"
+        title: "Mochila Viajera",
+        text: "Blog de viajes determinado por zonas, detalles de lugares que visitar e imagenes de estos. Realizado para el curso de YPF Ingenias. Tecnologias: HTML, CSS, Javascript.",
+        view: "https://carlabgo.github.io/MochilaViajera/",
+        source: "https://github.com/carlabgo/MochilaViajera"
     },
     {
-        imgsrc: img1,
-        title: "Gula Gula",
-        text: "Text text text",
-        view: "https://www.google.com/",
-        source: "https://www.google.com/"
-    },
-    {
-        imgsrc: img1,
-        title: "Gula Gula",
-        text: "Text text text",
-        view: "https://www.google.com/",
-        source: "https://www.google.com/"
+        imgsrc: img2,
+        title: "GulaGula",
+        text: "Web para buscar recetas en base a ingredientes. Proyecto final del curso full stack de Egg Educación. Tecnologias: Java, Spring boot, HTML, CSS, Bootstrap, Thymeleaf, MySQL.",
+        view: "",
+        source: "https://github.com/Nicolas-Ledesma/GulaGula/tree/main/GulaGula"
     },
 ];
 export default ProjectCardData
