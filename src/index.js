330b92af7c5bae4d1f49e4de5a2c63f74c9af832
carla <carlabgo95@gmail.com>
2022-12-27 03:01:33 -0300

    app.js changes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom"
+import {BrowserRouter} from "react-router-dom/BroswerRouter"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
 );
